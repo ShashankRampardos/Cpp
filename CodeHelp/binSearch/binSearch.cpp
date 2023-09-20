f84bc5ed6f5d94197c3f151fd31960e0c731872a
Shashank Vashistha <sharampardo@gmail.com>
2023-09-20 17:11:02 +0530

    Update binSearch.cpp

--- CodeHelp/binSearch/binSearch.cpp
+++ CodeHelp/binSearch/binSearch.cpp
@@ -8,7 +8,7 @@ bool binSearch(vector<int> &a, int target){
     int l=0,h=n-1;
     int mid=l+(h-l)/2;
     
-    while(l<=h){
+    while(l<=h){// ye equal karnay ka purpose keval ek he hay ki jab single element bacha ho tabh bhi loop may entry mil jaye, because single element wlaly case may bhi target mil sakta ha code may misbehiave without it.
         if(a[mid]>target)
         h=mid-1;
         else if(a[mid]<target)
