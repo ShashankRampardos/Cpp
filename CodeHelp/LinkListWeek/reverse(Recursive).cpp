bc814d309bd9aa957714221fbd191e2b595597dc
Shashank Vashistha <sharampardo@gmail.com>
2023-11-07 23:27:43 +0530

    Create reverse(Recursive).cpp

---
+++ CodeHelp/LinkListWeek/reverse(Recursive).cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    ListNode* recursive(ListNode* tp,ListNode* t){
+        if(t==NULL){
+            return tp;
+        }
+        ListNode *tn=t->next;
+        t->next=tp;
+        tp=t;
+        t=tn;
+        return recursive(tp,t);
+    }
+public:
+    ListNode* reverseList(ListNode* head) {
+        //temp prevous: tp
+        //temp temp current: t
+        ListNode *tp=NULL;
+        ListNode *t=head;
+
+       
+    head=recursive(tp,t);
+    return head;
+    }
+};
