156c561e6e2520b946a26230f565dc8aebf57bc6
Shashank Vashistha <sharampardo@gmail.com>
2023-11-15 14:46:55 +0530

    Create mergeNodesInBetweenZeros.cpp

---
+++ CodeHelp/LinkListWeek/mergeNodesInBetweenZeros.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+// nice TWO POINTER APPROACH
+//self explainatery.
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        ListNode* last=nullptr;
+        int sum=0;
+        fast=fast->next;
+        while(fast){
+        
+            if(fast->val!=0){
+                sum=sum+fast->val;
+             }else{
+                slow->val=sum;
+                sum=0;
+                last=slow;
+                slow=slow->next;
+             }
+             fast=fast->next;
+        }
+        last->next=nullptr;
+        return head;
+
+        while(slow){
+            ListNode* to_del=slow;
+            slow=slow->next;
+            delete to_del;
+        }
+    }
+};
