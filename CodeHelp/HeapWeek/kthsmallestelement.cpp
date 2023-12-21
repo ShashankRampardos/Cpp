1af6b5b11a08f649eedb5cd97ee12f6b2ba4a23a
Shashank Vashistha <sharampardo@gmail.com>
2023-12-21 12:11:03 +0530

    Create kthsmallestelement.cpp

---
+++ CodeHelp/HeapWeek/kthsmallestelement.cpp
@@ -0,0 +1,16 @@
+    int kthSmallest(int arr[], int l, int r, int k) {
+        priority_queue<int> maxh;
+        
+        for(int i=0;i<k;i++)
+        {
+            maxh.push(arr[i]);
+        }
+        for(int i=k;i<=r;i++){
+            if(arr[i]<maxh.top()){
+                maxh.pop();
+                maxh.push(arr[i]);
+            }
+        }
+        return maxh.top();
+    
+    }
