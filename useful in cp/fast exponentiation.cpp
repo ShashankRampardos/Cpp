972c5ba1dd438bd27c6531890334bf515d5036be
Shashank Vashistha <sharampardo@gmail.com>
2024-03-18 20:55:50 +0530

    Create fast exponentiation.cpp

---
+++ useful in cp/fast exponentiation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fastExpo(int a,int b){
+ int ans = 1;
+ while(b > 0){
+    if(b & 1)ans = ans*a;
+    a = a*a;
+    b >>= 1;
+ }
+ return ans;
+}
+int main(){
+  cout<<fastExpo(3,5);
+}
