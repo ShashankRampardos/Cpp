4c74dd8567d777b5eb26ce6ae8ede3001752f3eb
Shashank Vashistha <sharampardo@gmail.com>
2024-03-24 12:19:21 +0530

    Update global file contain all utilities and functions.cpp

--- useful in cp/global file contain all utilities and functions.cpp
+++ useful in cp/global file contain all utilities and functions.cpp
@@ -1,7 +1,25 @@
 
+#define mod 1000000007
+#define modSub(a,b) ((((a)%mod-(b)%mod)%mod+mod)%mod)
+#define modAdd(a,b) (((a)%(mod)+(b)%(mod))%(mod))
+#define modMul(a,b) ((((a)%(mod))*((b)%(mod)))%mod)
 
+#define ll long long
+
+//fast expo with modulo result
+ll powMod(ll a,ll b){
+    ll ans = 1;
+  while(b){
+    if(b&1)
+       ans = modMul(ans,a);
+
+    a = modMul(a,a);
+    b >>= 1;
+  }
+  return ans;
+}
 //fast exponentiation
-int fastExpo(int a,int b){
+int power(int a,int b){
  int ans = 1;
  while(b > 0){
     if(b & 1)ans = ans*a;
